b3f3639ead99f05043de91364bb01dfb7c2f3ada
Luciano S. Santana <lucianosantanabr@gmail.com>
2024-06-25 10:46:34 -0300

    Update PayloadRequest.java

--- src/main/java/com/spring/logstash/controller/PayloadRequest.java
+++ src/main/java/com/spring/logstash/controller/PayloadRequest.java
@@ -1,1 +1,22 @@
-package com.spring.logstash.controller;import java.io.Serializable;import lombok.AllArgsConstructor;import lombok.Builder;import lombok.Data;import lombok.NoArgsConstructor;import lombok.ToString;@Data@Builder@NoArgsConstructor@AllArgsConstructor@ToStringpublic class PayloadRequest implements Serializable {  private static final long serialVersionUID = 4877748064632930654L;  private String id;  private String message;}
+package com.spring.logstash.controller;
+
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class PayloadRequest implements Serializable {
+
+  private static final long serialVersionUID = 4877748064632930654L;
+
+  private String id;
+  private String message;
+
+}
